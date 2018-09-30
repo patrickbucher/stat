a1a2cdc0027ee1cb0b995b122b73d5c9faea294a
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2018-09-30 10:42:20 +0200

    2. Vorlesung: mit Aufgaben Serie 2 begonnen

---
+++ aufgaben/serie-02/aufgabe1.1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+marks = pd.Series([4.2, 2.3, 5.6, 4.5, 4.8, 3.9, 5.9, 2.4, 5.9, 6, 4, 3.7, 5, 5.2, 4.5, 3.6, 5, 6, 2.8, 3.3, 5.5, 4.2, 4.9, 5.1])
+mean_orig = marks.mean()
+median_orig = marks.median()
+
+print('original: mean={:.2f}, median={:.2f}'.format(mean_orig, median_orig))
+
+# a)
+marks_sorted = marks.sort_values()
+i_below_median = int(marks_sorted.count() / 2) - 1
+i_above_median = int(marks_sorted.count() / 2) + 1
+marks_sorted[i_below_median] = 1.0
+marks_sorted[i_below_median - 1] = 1.0
+marks_sorted[i_above_median] = 6.0
+mean_fake = marks_sorted.mean()
+median_fake = marks_sorted.median()
+
+print('fake: mean={:.2f}, median={:.2f}'.format(mean_fake, median_fake))
+
+# b)
+
+marks.plot(kind='box', title='Boxplot')
+plt.show()
+
+marks.plot(kind='hist', cumulative=True, histtype='step',
+        normed=True, bins=8, edgecolor='black')
+plt.show()
