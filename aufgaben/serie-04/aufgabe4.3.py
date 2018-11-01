1de054f406f58b74273704755d6e7765f1376fe7
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-11-01 14:48:47 +0100

    Serie 4, Aufgabe 3b

--- aufgaben/serie-04/aufgabe4.3.py
+++ aufgaben/serie-04/aufgabe4.3.py
@@ -11,4 +11,12 @@ sample_size = 25
 
 sd_sample = sd_population / sqrt(sample_size)
 prob_sample_mean_below_95 = norm.cdf(x=95, loc=mean_population, scale=sd_sample)
-print(prob_sample_mean_below_95)
+print('P(X<95)={:.4f}'.format(prob_sample_mean_below_95))
+
+# b)
+sample_size = 40
+mean_population = (4 + 6) / 2
+var_population = ((6 - 4) ** 2) / 12
+mean_x = mean_population
+var_x = sqrt(var_population) / sqrt(sample_size)
+print('μ={:.4f}, Var={:.4f}'.format(mean_x, var_x))
