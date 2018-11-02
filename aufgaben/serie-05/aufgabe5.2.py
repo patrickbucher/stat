7becd95b1385710fd8ea1e1a3279e6013169126d
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-11-02 06:52:42 +0100

    Serie 5, Aufgabe 2 a-c

---
+++ aufgaben/serie-05/aufgabe5.2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as st
+from pandas import Series, DataFrame
+
+values = np.array([0, 10, 11])
+
+# a)
+sim = Series(np.random.choice(values, size=1000, replace=True))
+plt.subplot(4, 2, 1)
+sim.hist(bins=[0, 1, 10, 11, 12], edgecolor='black')
+plt.title('Original')
+plt.subplot(4, 2, 2)
+st.probplot(sim, plot=plt)
+plt.title('Normal Q-Q Plot')
+
+# b)
+n = 5
+sim = np.random.choice(values, size=n*1000, replace=True)
+sim = DataFrame(np.reshape(sim, (n, 1000)))
+sim_mean = sim.mean()
+plt.subplot(4, 2, 3)
+sim_mean.hist(edgecolor='black')
+plt.title('Mittelwerte von 5 Beobachtungen')
+plt.subplot(4, 2, 4)
+st.probplot(sim_mean, plot=plt)
+plt.title('Normal Q-Q Plot')
+
+# c)
+i = 5
+ns = [10, 200]
+for n in ns:
+    sim = np.random.choice(values, size=n*1000, replace=True)
+    sim = DataFrame(np.reshape(sim, (n, 1000)))
+    sim_mean = sim.mean()
+    plt.subplot(4, 2, i)
+    sim_mean.hist(edgecolor='black')
+    plt.title('Mittelwerte von {} Beobachtungen'.format(n))
+    i += 1
+    plt.subplot(4, 2, i)
+    st.probplot(sim_mean, plot=plt)
+    plt.title('Normal Q-Q Plot')
+    i += 1
+
+plt.tight_layout()
+plt.show()
+
+# d)
+e_x = values.sum() / values.size
+print('E[Xn]={}'.format(e_x))
+# TODO: Var[Xn]
