88b642dd4da8bb4cbe494027e1cc3cdebb97a293
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2018-11-02 12:21:13 +0100

    Serie 5, Aufgabe 3 b-c

--- aufgaben/serie-05/aufgabe5.3.py
+++ aufgaben/serie-05/aufgabe5.3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as st
 
 n_mice = 54
 n_groups = 3
@@ -13,5 +15,28 @@ dose_low = 0.3
 iron = pd.read_table('./iron.dat', sep=' ', index_col=False)
 
 # a)
-iron.plot(kind='box')
+plt.subplot(1, 2, 1)
+iron.plot(kind='box', ax=plt.gca())
+plt.ylabel('iron')
+# Je kleiner die Dosis, desto grösser fällt die Streuung aus.
+
+# b)
+plt.subplot(1, 2, 2)
+np.log(iron).plot(kind='box', ax=plt.gca())
+plt.ylabel('log(iron)')
+# Durch das Logarithmieren verschwinden die Ausreisser. Die Streuung der
+# Gruppen gleicht sich einander an.
+
+plt.tight_layout()
 plt.show()
+
+# c)
+plt.subplot(1, 2, 1)
+st.probplot(iron['medium'], plot=plt)
+plt.ylabel('iron["medium"]')
+
+plt.subplot(1, 2, 2)
+st.probplot(np.log(iron['medium']), plot=plt)
+plt.ylabel('log(iron["medium"])')
+plt.show()
+# Die Normalverteilung passt besser auf den logarithmierten Plot.
