1a252c434f681ebc5b78d150d172427f540ea912
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-11-24 10:40:49 +0100

    more examples on bootstrapping

--- aufgaben/serie-07/bootstrapping.py
+++ aufgaben/serie-07/bootstrapping.py
@@ -40,3 +40,58 @@ xbarstar.plot(kind='hist', edgecolor='black')
 plt.subplot(122)
 probplot(xbarstar, plot=plt)
 plt.show()
+
+# TODO:
+x = np.random.normal(loc=40, scale=5, size=100)
+n = x.size
+np.random.seed(4)
+xbar = x.mean()
+nboot = 20
+bootstrap_samples = np.random.choice(x, n*nboot, replace=True)
+bootstrap_sample_array = np.reshape(bootstrap_samples, (n, nboot))
+xbarstar = bootstrap_sample_array.mean(axis=0)
+deltastar = xbarstar - xbar
+print('deltastar={}'.format(deltastar))
+d = np.percentile(deltastar, q=[2.5, 97.5])
+ci = xbar - [d[1], d[0]]
+print('confidence interval: {}'.format(ci))
+
+# TODO:
+x = np.random.normal(loc=40, scale=5, size=100000)
+measurement_array = np.reshape(x, (1000, 100))
+print(measurement_array.shape)
+print(measurement_array[1].size)
+nboot = 1000
+n = 100
+k = 0
+for i in range(0, 1000):
+    x = measurement_array[i]
+    xbar = x.mean()
+    bootstrap_samples = np.random.choice(x, n*nboot, replace=True)
+    bootstrap_sample_array = np.reshape(bootstrap_samples, (n, nboot))
+    xbarstar = bootstrap_sample_array.mean(axis=0)
+    deltastar = xbarstar - xbar
+    d = np.percentile(deltastar, q=[2.5, 97.5])
+    if xbar-d[0] >= 40 >= xbar-d[1]:
+        k += 1
+print('k={:d}'.format(k))
+
+# TODO:
+x = np.random.normal(loc=40, scale=100, size=10000)
+measurement_array = np.reshape(x, (100, 100))
+print(measurement_array.shape)
+nboot = 10000
+n = 100
+for i in range(0, 100):
+    y = measurement_array[i]
+    xbar = y.mean()
+    tmpdata = np.random.choice(y, n*nboot, replace=True)
+    bootstrapsample = np.reshape(tmpdata, (n, nboot))
+    xbarstar = bootstrapsample.mean(axis=0)
+    deltastar = xbarstar - xbar
+    d = np.percentile(deltastar, q=[2.5, 97.5])
+    plt.plot([i, i], [xbar-d[1], xbar-d[0]])
+    if (xbar-d[1] <= 40 <= xbar-d[0]) == False:
+        plt.plot([i, i], [xbar-d[1], xbar-d[0]], c='black', linewidth=3)
+plt.plot([-5, 105], [40,40])
+plt.show()
